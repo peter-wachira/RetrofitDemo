92999c1791eb135ce04b517feba2292b9b1c0bfb
PeteHack <pwachira900@gmail.com>
2020-11-26 13:07:05 +0300

    add connection timeouts

--- app/src/main/java/com/anushka/retrofitdemo/AlbumService.kt
+++ app/src/main/java/com/anushka/retrofitdemo/AlbumService.kt
@@ -5,13 +5,24 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
+
 interface AlbumService {
-    @GET("/albums")
-    suspend fun getAlbums():Response<Albums>
 
-    @GET("/albums")
-    suspend fun getSortedAlbums(@Query("userId")userId:Int):Response<Albums>
+   @GET("/albums")
+   suspend fun getAlbums() :Response<Albums>
 
-    @GET
-    suspend fun getSingleAlbum(@Path(value = "id") albumId:Int): Response<AlbumsItem>
+   @GET("/albums")
+   suspend fun getSortedAlbums(@Query("userId") userId:Int) :Response<Albums>
+
+   @GET("/albums/{id}")
+   suspend fun getAlbum(@Path(value = "id")albumId:Int) : Response<AlbumsItem>
+
+
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/anushka/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/anushka/retrofitdemo/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.anushka.retrofitdemo
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.TokenWatcher
 import android.util.Log
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.liveData
@@ -11,45 +12,51 @@ import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var retService: AlbumService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val retService = RetrofitInstance
+        retService = RetrofitInstance
             .getRetrofitInstance()
             .create(AlbumService::class.java)
+        getRequestWithQueryParameters()
+        //getRequestWithPathParameters()
 
+    }
+
+
+    private fun getRequestWithQueryParameters() {
         val responseLiveData: LiveData<Response<Albums>> = liveData {
             val response = retService.getSortedAlbums(3)
             emit(response)
         }
-
         responseLiveData.observe(this, Observer {
-            val albumList = it.body()?.listIterator()
-            if (albumList != null) {
-                while (albumList.hasNext()) {
-                    val albumsItem = albumList.next()
-                    Log.i("MYTAG", albumsItem.title)
-                    val  result = " "+"AlbumTitle : ${albumsItem.title}"+"\n" +
-                                  " "+"Album id : ${albumsItem.id}"+"\n" +
-                                  " "+"User id : ${albumsItem.userId}"+"\n" +"\n\n\n\n"
+            val albumsList = it.body()?.listIterator()
+            if (albumsList != null) {
+                while (albumsList.hasNext()) {
+                    val albumsItem = albumsList.next()
+                    val result = " " + "Album Title : ${albumsItem.title}" + "\n" +
+                            " " + "Album id : ${albumsItem.id}" + "\n" +
+                            " " + "User id : ${albumsItem.userId}" + "\n\n\n"
                     text_view.append(result)
-
                 }
             }
         })
+    }
 
-//        get single album
-        val pathResponse : LiveData <Response<AlbumsItem>> = liveData {
-            val response = retService.getSingleAlbum(4)
+
+    private fun getRequestWithPathParameters() {
+        val pathResponse: LiveData<Response<AlbumsItem>> = liveData {
+            val response = retService.getAlbum(3)
             emit(response)
+
         }
 
         pathResponse.observe(this, Observer {
             val title = it.body()?.title
-            Toast.makeText(this, title, Toast.LENGTH_LONG).show()
+            Toast.makeText(applicationContext, title, Toast.LENGTH_LONG).show()
         })
-
     }
+
+
 }
